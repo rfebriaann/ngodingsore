4cbce70c5aba9d63cdcc53f471390cb9174cc6a5
rfebriaann <raflyfebriann@gmail.com>
2024-12-30 18:07:36 +0700

    commit work update

--- pages/component/works.js
+++ pages/component/works.js
@@ -8,10 +8,17 @@ export default function Works() {
   const containerRef = useRef(null);
   const garissatu = useRef(null);
   const garisdua = useRef(null);
+  const garistiga = useRef(null);
+  const garisempat = useRef(null);
+  const garislima = useRef(null);
+  const garisenam = useRef(null);
+  const garistujuh = useRef(null);
+  const garisdelapan = useRef(null);
+  // const garissembilan = useRef(null);
 
   useEffect(() => {
     let sections = gsap.utils.toArray(".panel");
-    const totalPanels = 5; // Jumlah total panel
+    const totalPanels = 5; 
     const screenWidth = window.innerWidth;
 
     gsap.fromTo(
@@ -63,7 +70,7 @@ export default function Works() {
           trigger: garisdua.current,
           start: "top +=300",
           end: "center +=500",
-          markers: true,
+          // markers: true,
           scrub: 1,
         },
         duration: 3,
@@ -71,6 +78,209 @@ export default function Works() {
         ease: "power1.in",
       }
     );
+
+    gsap.fromTo(
+      garistiga.current,
+      1,
+      {
+        scrollTrigger: {
+          trigger: garistiga.current,
+          start: "top +=300",
+          end: "bottom +=500",
+          // markers: true,
+          scrub: 1,
+        },
+        duration: 3,
+        width: 0,
+        ease: "back.in",
+      },
+      {
+        scrollTrigger: {
+          trigger: garistiga.current,
+          start: "top +=300",
+          end: "bottom +=500",
+          // markers: true,
+          scrub: 1,
+        },
+        duration: 3,
+        width: 900,
+        ease: "power1.in",
+      }
+    );
+
+    gsap.fromTo(
+      garisempat.current,
+      1,
+      {
+        scrollTrigger: {
+          trigger: garisempat.current,
+          start: "top +=300",
+          end: "center +=500",
+          // markers: true,
+          scrub: 1,
+        },
+        duration: 3,
+        width: 0,
+        ease: "back.in",
+      },
+      {
+        scrollTrigger: {
+          trigger: garisempat.current,
+          start: "top +=300",
+          end: "center +=500",
+          // markers: true,
+          scrub: 1,
+        },
+        duration: 3,
+        width: 1000,
+        ease: "power1.in",
+      }
+    );
+
+    gsap.fromTo(
+      garislima.current,
+      1,
+      {
+        scrollTrigger: {
+          trigger: garislima.current,
+          start: "top +=300",
+          end: "bottom +=500",
+          // markers: true,
+          scrub: 1,
+        },
+        duration: 3,
+        width: 0,
+        ease: "back.in",
+      },
+      {
+        scrollTrigger: {
+          trigger: garislima.current,
+          start: "top +=300",
+          end: "bottom +=500",
+          // markers: true,
+          scrub: 1,
+        },
+        duration: 3,
+        width: 900,
+        ease: "power1.in",
+      }
+    );
+
+    gsap.fromTo(
+      garisenam.current,
+      1,
+      {
+        scrollTrigger: {
+          trigger: garisenam.current,
+          start: "top +=300",
+          end: "center +=500",
+          // markers: true,
+          scrub: 1,
+        },
+        duration: 3,
+        width: 0,
+        ease: "back.in",
+      },
+      {
+        scrollTrigger: {
+          trigger: garisenam.current,
+          start: "top +=300",
+          end: "center +=500",
+          // markers: true,
+          scrub: 1,
+        },
+        duration: 3,
+        width: 1000,
+        ease: "power1.in",
+      }
+    );
+
+    gsap.fromTo(
+      garistujuh.current,
+      1,
+      {
+        scrollTrigger: {
+          trigger: garistujuh.current,
+          start: "top +=300",
+          end: "bottom +=500",
+          // markers: true,
+          scrub: 1,
+        },
+        duration: 3,
+        width: 0,
+        ease: "back.in",
+      },
+      {
+        scrollTrigger: {
+          trigger: garistujuh.current,
+          start: "top +=300",
+          end: "bottom +=500",
+          // markers: true,
+          scrub: 1,
+        },
+        duration: 3,
+        width: 900,
+        ease: "power1.in",
+      }
+    );
+
+    gsap.fromTo(
+      garisdelapan.current,
+      1,
+      {
+        scrollTrigger: {
+          trigger: garisdelapan.current,
+          start: "top +=300",
+          end: "center +=500",
+          // markers: true,
+          scrub: 1,
+        },
+        duration: 3,
+        width: 0,
+        ease: "back.in",
+      },
+      {
+        scrollTrigger: {
+          trigger: garisdelapan.current,
+          start: "top +=300",
+          end: "center +=500",
+          // markers: true,
+          scrub: 1,
+        },
+        duration: 3,
+        width: 1000,
+        ease: "power1.in",
+      }
+    );
+
+    // gsap.fromTo(
+    //   garissembilan.current,
+    //   1,
+    //   {
+    //     scrollTrigger: {
+    //       trigger: garissembilan.current,
+    //       start: "top +=300",
+    //       end: "bottom +=500",
+    //       // markers: true,
+    //       scrub: 1,
+    //     },
+    //     duration: 3,
+    //     width: 0,
+    //     ease: "back.in",
+    //   },
+    //   {
+    //     scrollTrigger: {
+    //       trigger: garissembilan.current,
+    //       start: "top +=300",
+    //       end: "bottom +=500",
+    //       markers: true,
+    //       scrub: 1,
+    //     },
+    //     duration: 3,
+    //     width: 900,
+    //     ease: "power1.in",
+    //   }
+    // );
 
     // Horizontal scroll animation
     gsap.to(sections, {
@@ -106,10 +316,10 @@ export default function Works() {
               </h1>
             </div>
             <div className="relative">
-              <h1 className="relative z-20 font-figtree text-[80px] font-medium">
+              <h1 className="relative z-20 font-figtree sm:text-[28px] md:text-[80px] font-medium">
                 Social Media Marketing
               </h1>
-              <div className="absolute z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[600px] h-[600px] bg-red-500">
+              <div className="absolute z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 sm:w-[300px] md:w-[600px] sm:h-[500px] md:h-[600px] bg-red-500">
                 <Image
                   src="/gif/social.webp" // Path ke file WebP Anda
                   alt="Example WebP"
@@ -119,7 +329,7 @@ export default function Works() {
               </div>
               <div
                 ref={garissatu}
-                className="absolute top-1/2 left-1/3 translate-x-1/2 -translate-y-1/2 w-[200px] h-10 bg-red-500"
+                className="absolute top-1/2 left-1/3 translate-x-1/2 -translate-y-1/2 w-[200px] h-10 bg-[#FF613E]"
               ></div>
             </div>
           </div>
@@ -132,10 +342,10 @@ export default function Works() {
               </h1>
             </div>
             <div className="relative ">
-              <h1 className="relative z-20 font-figtree text-[80px] font-medium">
+              <h1 className="relative z-20 font-figtree sm:text-[28px] md:text-[80px] font-medium">
                 Branding
               </h1>
-              <div className="absolute z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[600px] h-[600px] bg-red-500">
+              <div className="absolute z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 sm:w-[300px] md:w-[600px] sm:h-[500px] md:h-[600px] bg-red-500">
                 <Image
                   src="/gif/branding.webp" // Path ke file WebP Anda
                   alt="Example WebP"
@@ -145,7 +355,11 @@ export default function Works() {
               </div>
               <div
                 ref={garisdua}
-                className="absolute top-1/2 -left-1/3 -translate-x-1/2 -translate-y-1/2 w-[0px] h-10 bg-red-500"
+                className="absolute top-1/2 -left-1/3 -translate-x-1/2 -translate-y-1/2 w-[0px] h-10 bg-[#FF613E]"
+              ></div>
+              <div
+                ref={garistiga}
+                className="absolute top-1/2 left-0 translate-x-1/2 -translate-y-1/2 w-[900px] h-10 bg-[#FF613E]"
               ></div>
             </div>
           </div>
@@ -158,10 +372,10 @@ export default function Works() {
               </h1>
             </div>
             <div className="relative">
-              <h1 className="relative z-10 font-figtree text-[80px] font-medium">
+              <h1 className="relative  z-20 font-figtree sm:text-[28px] md:text-[80px] font-medium">
                 Production
               </h1>
-              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[600px] h-[600px] bg-red-500">
+              <div className="absolute z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  sm:w-[300px] md:w-[600px] sm:h-[500px] md:h-[600px] bg-red-500">
                 <Image
                   src="/gif/production.webp" // Path ke file WebP Anda
                   alt="Example WebP"
@@ -169,6 +383,14 @@ export default function Works() {
                   objectFit="cover"
                 />
               </div>
+              <div
+                ref={garisempat}
+                className="absolute top-1/2 -left-1/3 -translate-x-1/2 -translate-y-1/2 w-[0px] h-10 bg-[#FF613E]"
+              ></div>
+              <div
+                ref={garislima}
+                className="absolute top-1/2 left-0 translate-x-1/2 -translate-y-1/2 w-[900px] h-10 bg-[#FF613E]"
+              ></div>
             </div>
           </div>
         </div>
@@ -180,10 +402,10 @@ export default function Works() {
               </h1>
             </div>
             <div className="relative">
-              <h1 className="relative z-10 font-figtree text-[80px] font-medium">
+              <h1 className="relative z-20 font-figtree sm:text-[28px] md:text-[80px] font-medium">
                 Software House
               </h1>
-              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[600px] h-[600px] bg-red-500">
+              <div className="absolute z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  sm:w-[300px] md:w-[600px] sm:h-[500px] md:h-[600px] bg-red-500">
                 <Image
                   src="/gif/coding.webp" // Path ke file WebP Anda
                   alt="Example WebP"
@@ -191,6 +413,14 @@ export default function Works() {
                   objectFit="cover"
                 />
               </div>
+              <div
+                ref={garisenam}
+                className="absolute top-1/2 -left-1/3 -translate-x-1/2 -translate-y-1/2 w-[0px] h-10 bg-[#FF613E]"
+              ></div>
+              <div
+                ref={garistujuh}
+                className="absolute top-1/2 left-0 translate-x-1/2 -translate-y-1/2 w-[900px] h-10 bg-[#FF613E]"
+              ></div>
             </div>
           </div>
         </div>
@@ -202,10 +432,10 @@ export default function Works() {
               </h1>
             </div>
             <div className="relative">
-              <h1 className="relative z-10 font-figtree text-[80px] font-medium">
+              <h1 className="relative z-20 font-figtree sm:text-[28px] md:text-[80px] font-medium">
                 Photography
               </h1>
-              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[600px] h-[600px] bg-red-500">
+              <div className="absolute z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  sm:w-[300px] md:w-[600px] sm:h-[500px] md:h-[600px] bg-red-500">
                 <Image
                   src="/gif/photo.webp" // Path ke file WebP Anda
                   alt="Example WebP"
@@ -213,6 +443,14 @@ export default function Works() {
                   objectFit="cover"
                 />
               </div>
+              <div
+                ref={garisdelapan}
+                className="absolute top-1/2 -left-1/3 -translate-x-1/2 -translate-y-1/2 w-[0px] h-10 bg-[#FF613E]"
+              ></div>
+              {/* <div
+                ref={garissembilan}
+                className="absolute top-1/2 left-0 translate-x-1/2 -translate-y-1/2 w-[900px] h-10 bg-[#FF613E]"
+              ></div> */}
             </div>
           </div>
         </div>
